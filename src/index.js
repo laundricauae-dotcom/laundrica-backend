f2dc5c9f14f9b8305f2d5e4bc905924a7f6cc74a
rohan654321 <mondalrohan201@gmail.com>
2026-04-12 23:49:33 +0530

    update

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ Handle preflight requests
-app.options('*', cors(corsOptions));
+app.options('*', cors());
 
 
 // ==================== MIDDLEWARE ====================
